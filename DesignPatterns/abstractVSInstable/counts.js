d1ff468065c68f022aeea0958d9da35a86f7ec64
soccerKevin <soccerkevint11@gmail.com>
2022-07-17 21:48:39 -0700

    module abstractness

--- DesignPatterns/abstractVSInstable/counts.js
+++ DesignPatterns/abstractVSInstable/counts.js
@@ -1,11 +1,8 @@
-const { existsSync, promises } = require('fs')
+const { existsSync, readFileSync, promises: fsPromises } = require('fs')
 const glob = require('glob')
 
-const {
-  NEO4J_PATH,
-  SRC_PATH,
-  JAVA_PATH
-} = require('./const')
+const nodePath = require('path')
+const NEO4J_PATH = nodePath.join(__dirname, 'neo4j/community')
 
 const getFilesIn = async ({ path, match='/**', type=null }) => {
   let directories = null
@@ -15,7 +12,7 @@ const getFilesIn = async ({ path, match='/**', type=null }) => {
     throw new Error('No such path')
   }
 
-  const stat = await promises.lstat(statPath)
+  const stat = await fsPromises.lstat(statPath)
   if (!stat.isDirectory())
     throw new Error('No such directory')
 
@@ -23,27 +20,27 @@ const getFilesIn = async ({ path, match='/**', type=null }) => {
 
   if (type == 'd')
     return filePaths.filter(async (filePath) => {
-      const stat = await promises.lstat(filePath)
+      const stat = await fsPromises.lstat(filePath)
       return stat.isDirectory()
     })
 
   if (type == 'f')
     return filePaths.filter(async (filePath) => {
-      const stat = await promises.lstat(filePath)
+      const stat = await fsPromises.lstat(filePath)
       return !stat.isDirectory()
     })
 
   return filePaths
 }
 
-const getModuleFiles = async (modulePath) => {
+const getModuleCodeFiles = async (modulePath) => {
   try {
     const javaFiles = await getFilesIn({ path: modulePath, match: '**/**.java', type: 'f'})
     const scalaFiles = await getFilesIn({ path: modulePath, match: '**/**.scala', type: 'f'})
-    const components = javaFiles.concat(scalaFiles)
-    return components
+    const files = javaFiles.concat(scalaFiles)
+    return files
   }
-  catch (err){
+  catch (err) {
     if (err.message == 'No such path')
       console.log('No such path: ', modulePath)
 
@@ -55,11 +52,35 @@ const getModuleFiles = async (modulePath) => {
   }
 }
 
+const isAbstract = async (filePath) => {
+  const contents = readFileSync(filePath, { encoding: 'utf8', flag: 'r' })
+  const lowerContents = contents.toLowerCase()
+  const isAbstract = lowerContents.indexOf('abstract') > -1
+  const isInterface = lowerContents.indexOf('interface') > -1
+  return isAbstract || isInterface
+}
+
+const moduleAbstractness = async (modulePath) => {
+  const mFiles = await getModuleCodeFiles(modulePath)
+  const files = []
+  await mFiles.map(async (filePath) => {
+    const abstract = await isAbstract(filePath)
+    files.push({ path: filePath, isAbstract: abstract })
+  })
+
+  const abstractCount = files.filter((f) => f.isAbstract).length
+  const concreteCount = files.length - abstractCount
+  return abstractCount / concreteCount
+}
+
 const run = async () => {
   const modules = await getFilesIn({ path: NEO4J_PATH, match: '/*/', type: 'd' })
-  console.log('modules: ', modules)
 
-  modules.forEach(async (m) => getModuleFiles(m))
+  modules.forEach(async (m) => {
+    const ma = await moduleAbstractness(m)
+    console.log(m, ': ', ma)
+  })
+
 }
 
 run()
