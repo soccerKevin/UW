e76e91868707dd9b064dd2caf0a667067c963014
soccerKevin <soccerkevint11@gmail.com>
2022-07-17 23:28:18 -0700

    getting there

--- DesignPatterns/abstractVSInstable/counts.js
+++ DesignPatterns/abstractVSInstable/counts.js
@@ -1,8 +1,17 @@
 const { existsSync, readFileSync, promises: fsPromises } = require('fs')
+const { XMLParser } = require('fast-xml-parser')
+var _ = require('lodash');
+const xmlParser = new XMLParser()
 const glob = require('glob')
+const Bluebird = require('bluebird')
 
 const nodePath = require('path')
 const NEO4J_PATH = nodePath.join(__dirname, 'neo4j/community')
+
+const modulesFinal = {}
+const incomingDependencies = {}
+const outgoingDependencies = {}
+const modulesAbstractness = {}
 
 const getFilesIn = async ({ path, match='/**', type=null }) => {
   let directories = null
@@ -52,12 +61,13 @@ const getModuleCodeFiles = async (modulePath) => {
   }
 }
 
+const getFileContents = (filePath) =>
+  readFileSync(filePath, { encoding: 'utf8', flag: 'r' })
+
 const isAbstract = async (filePath) => {
-  const contents = readFileSync(filePath, { encoding: 'utf8', flag: 'r' })
+  const contents = getFileContents(filePath)
   const lowerContents = contents.toLowerCase()
-  const isAbstract = lowerContents.indexOf('abstract') > -1
-  const isInterface = lowerContents.indexOf('interface') > -1
-  return isAbstract || isInterface
+  return !!lowerContents.match(/(public|protected) (abstract|interface)/gm)
 }
 
 const moduleAbstractness = async (modulePath) => {
@@ -70,17 +80,54 @@ const moduleAbstractness = async (modulePath) => {
 
   const abstractCount = files.filter((f) => f.isAbstract).length
   const concreteCount = files.length - abstractCount
-  return abstractCount / concreteCount
+  const abstractness = abstractCount / concreteCount
+  return { abstractCount, concreteCount, abstractness }
 }
+
+const setModuleDepencies = async (modulePath) => {
+  const pomFiles = await getFilesIn({ path: modulePath, match: '**/pom.xml', type: 'f'})
+  const xml = getFileContents(pomFiles[0])
+  const parsedXML = xmlParser.parse(xml)
+
+  const dependencies = [parsedXML.project.dependencies.dependency].flat()
+  const artifactIds = dependencies.map((d) => d.artifactId.replace('neo4j-', ''))
+  console.log('artifactIds: ', artifactIds)
+  const name = getModuleName(modulePath)
+  modulesFinal[name].incomingDependencies = [artifactIds]
+
+  artifactIds.forEach((dependency) => {
+    if (!modulesFinal[dependency]) return
+    modulesFinal[dependency].outgoingDependencies.push(name)
+  })
+  // console.log('artifactIds: ', artifactIds)
+  // console.log('parsedXML: ', parsedXML)
+}
+
+const getModuleName = (modulePath) => modulePath.split('/').slice(-2, -1)[0].replace('neo4j-', '')
 
 const run = async () => {
   const modules = await getFilesIn({ path: NEO4J_PATH, match: '/*/', type: 'd' })
-
-  modules.forEach(async (m) => {
-    const ma = await moduleAbstractness(m)
-    console.log(m, ': ', ma)
+  modules.forEach((mPath) => {
+    modulesFinal[getModuleName(mPath)] = {
+      incomingDependencies: [],
+      outgoingDependencies: []
+    }
   })
 
+  await Bluebird.map(modules, async (mPath) => {
+    const name = getModuleName(mPath)
+    setModuleDepencies(mPath)
+    const { abstractCount, concreteCount, abstractness } = await moduleAbstractness(mPath)
+    const mf = modulesFinal[name]
+    mf.abstractCount = abstractCount
+    mf.concreteCount = concreteCount
+    mf.abstractness = abstractness
+    const inDep = _.uniq(mf['incomingDependencies']).length
+    const outDep = _.uniq(mf['outgoingDependencies']).length
+    mf.instability = inDep / (inDep + outDep)
+  })
+
+  console.log('modulesFinal: ', modulesFinal)
 }
 
 run()
